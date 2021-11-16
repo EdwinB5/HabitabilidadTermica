62a547256f3e56a135b13f844ac0c8ec043cd1de
EdwinB5 <edwin.becerra@uniminuto.edu.co>
2021-11-16 14:37:14 -0500

    Se recuperan los datos del formulario y se envian al controlador

--- app/core/app.py
+++ app/core/app.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from .controller import controller
 
 app = Flask(__name__, template_folder='view', static_folder='view/static/')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def datos_entrada():
 	'''
 	Vista principal del proyecto, formulario de entrada
 	'''
 	return render_template('index.html')
 
-@app.route('/salida')
+@app.before_request
+def obtener_datos():
+	'''
+	Obteniendo datos del formulario y enviandolos al controlador
+	para empezar la evaluacion de habitabilidad
+	'''
+	if request.method == 'POST':
+		estacion = request.form.get('estacion')
+		humedad = request.form.get('humedad')
+		temperatura_ambiente = request.form.get('temperatura')
+		tamaño_edificacion = request.form.get('tamaño')
+		coeficiente = request.form.get('coeficiente')
+		controller.datos_controller(estacion, humedad, temperatura_ambiente, tamaño_edificacion, coeficiente)
+
+@app.route('/salida', methods=['GET', 'POST'])
 def datos_salida():
 	'''
 	Vista salida del proyecto, plantilla salida
 	'''
+
 	return render_template('resultado.html')
 
 # Web services
---
+++ app/core/controller/controller.py
@@ -0,0 +1,10 @@
+def datos_controller(estacion, humedad, temperatura_ambiente, tamaño_edificacion, coeficiente):
+	'''
+	Esta función conecta la entrada de datos con
+	los modelos
+	'''
+	print(estacion)
+	print(humedad)
+	print(temperatura_ambiente)
+	print(coeficiente)
+	print(tamaño_edificacion)
